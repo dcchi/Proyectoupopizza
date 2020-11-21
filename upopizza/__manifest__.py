4cb3deb021d053e919efdc18495b0a8afbbdcfb6
dcchi <dcchi@alu.upo.es>
2020-11-21 12:32:22 +0100

    Modif. Mañana 21/11/2020

--- upopizza/__manifest__.py
+++ upopizza/__manifest__.py
@@ -16,7 +16,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Upopizza',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -27,12 +27,11 @@
         'security/security.xml',
         'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml'
-   
+        'views/cliente_view.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
-    ],'application': True,
-    
+    ], 'application': True,
+
 }
